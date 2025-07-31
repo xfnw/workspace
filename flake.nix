cb88e777801791db2b27742caff314bb03768d55
xfnw <xfnw@riseup.net>
2025-07-30 23:12:40 -0400

    flake: create
    just a simple shell for now

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc rustfmt cargo clippy ];
+        };
+      });
+}
