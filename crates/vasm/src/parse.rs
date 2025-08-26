0b67f7bed951325154708d6cb03199db0405f818
xfnw <xfnw@riseup.net>
2025-08-26 13:11:56 -0400

    vasm: parse an operand

--- crates/vasm/src/parse.rs
+++ crates/vasm/src/parse.rs
@@ -1,3 +1,15 @@
+use nom::{
+    IResult, Parser,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, alphanumeric1, one_of},
+    combinator::{map, map_res, opt, recognize, value},
+    multi::{many0, many1},
+    sequence::{delimited, pair, preceded},
+};
+
+use crate::repr::{LabelOffset, Operand};
+
 use super::repr;
 
 #[derive(Debug, foxerror::FoxError)]
@@ -5,8 +17,94 @@ pub enum Error {
     /// could not build internal representation
     #[err(from)]
     Repr(repr::Error),
+    /// could not parse
+    #[err(from)]
+    Parse(nom::Err<nom::error::Error<String>>),
 }
 
+fn hexadecimal_value(inp: &str) -> IResult<&str, u16> {
+    map_res(
+        preceded(tag("$"), recognize(many1(one_of("0123456789abcdefABCDEF")))),
+        |out| u16::from_str_radix(out, 16),
+    )
+    .parse(inp)
+}
+
+fn decimal_value(inp: &str) -> IResult<&str, u16> {
+    map_res(recognize(many1(one_of("0123456789"))), |out: &str| {
+        out.parse::<u16>()
+    })
+    .parse(inp)
+}
+
+fn number_value(inp: &str) -> IResult<&str, u16> {
+    alt((hexadecimal_value, decimal_value)).parse(inp)
+}
+
+fn number_offset(inp: &str) -> IResult<&str, repr::Offset> {
+    map_res(
+        pair(
+            alt((value(false, tag("+")), value(true, tag("-")))),
+            number_value,
+        ),
+        |(neg, num)| {
+            num.try_into()
+                .map(|n| repr::Offset::new(if neg { 0 - n } else { n }))
+        },
+    )
+    .parse(inp)
+}
+
+fn label_name(inp: &str) -> IResult<&str, &str> {
+    recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0(alt((alphanumeric1, tag("_")))),
+    ))
+    .parse(inp)
+}
+
+fn label_offset(inp: &str) -> IResult<&str, LabelOffset> {
+    alt((
+        map(pair(opt(label_name), number_offset), |(name, offset)| {
+            LabelOffset::new(name.map(str::to_string), offset)
+        }),
+        map(label_name, |name| {
+            LabelOffset::new(Some(name.to_string()), repr::Offset::new(0))
+        }),
+    ))
+    .parse(inp)
+}
+
+fn operand(inp: &str) -> IResult<&str, Operand> {
+    alt((
+        value(Operand::AtXInc, tag("[X]+")),
+        value(Operand::AtYInc, tag("[Y]+")),
+        value(Operand::AtX, tag("[X]")),
+        value(Operand::AtY, tag("[Y]")),
+        map(preceded(tag("#"), number_value), Operand::new_immediate),
+        map(number_value, |v| Operand::Mem(repr::MemoryAddress::new(v))),
+        map(
+            delimited(tag("[SP"), number_offset, tag("]")),
+            Operand::AtSPn,
+        ),
+        map(delimited(tag("[X"), number_offset, tag("]")), Operand::AtXn),
+        map(delimited(tag("[Y"), number_offset, tag("]")), Operand::AtYn),
+        map(preceded(tag("SP"), number_offset), Operand::SPn),
+        value(Operand::A, tag("A")),
+        value(Operand::B, tag("B")),
+        value(Operand::C, tag("C")),
+        value(Operand::D, tag("D")),
+        value(Operand::X, tag("X")),
+        value(Operand::Y, tag("Y")),
+        value(Operand::PC, tag("PC")),
+        value(Operand::SP, tag("SP")),
+        map(label_offset, Operand::Rel2),
+    ))
+    .parse(inp)
+}
+
+#[allow(clippy::redundant_closure_for_method_calls)]
 pub fn parse(inp: &str) -> Result<Vec<repr::Opcode>, Error> {
+    dbg!(operand(inp).map_err(|e| e.to_owned())?);
     todo!()
 }
