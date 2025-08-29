b591adc79f56fa7ec5875b8672ab13289b899a12
xfnw <xfnw@riseup.net>
2025-08-29 09:26:39 -0400

    vasm: Display error messages

--- crates/vasm/src/main.rs
+++ crates/vasm/src/main.rs
@@ -1,5 +1,5 @@
 use argh::{FromArgs, from_env};
-use std::path::PathBuf;
+use std::{path::PathBuf, process::ExitCode};
 
 mod assemble;
 mod parse;
@@ -29,12 +29,11 @@ enum Error {
     Assemble(assemble::Error),
 }
 
-fn main() -> Result<(), Error> {
-    let opt: Opt = from_env();
+fn run(opt: &Opt) -> Result<(), Error> {
     let input = std::fs::read_to_string(&opt.file)?;
     let assembled = assemble::assemble(parse::parse(&input)?)?;
 
-    if let Some(output) = opt.output {
+    if let Some(output) = &opt.output {
         let bytes: Vec<_> = assembled.into_iter().flat_map(u16::to_be_bytes).collect();
         std::fs::write(output, bytes)?;
     } else {
@@ -44,4 +43,15 @@ fn main() -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+fn main() -> ExitCode {
+    let opt: Opt = from_env();
+
+    if let Err(e) = run(&opt) {
+        eprintln!("{e}");
+        return ExitCode::FAILURE;
+    }
+
+    ExitCode::SUCCESS
 }
