24935e95c92f0335b3f22bec5de78f423d722fe4
xfnw <xfnw@riseup.net>
2025-09-05 20:51:10 -0400

    vasm: add more h to test

--- crates/vasm/tests/data/hwrite.asm
+++ crates/vasm/tests/data/hwrite.asm
@@ -2,7 +2,10 @@
 	sys 0		; write to tele
 	move A, cmsg	; text to send
 	sys 0		; write to tele
+	move A, hmsg	; text to send
+	sys $25		; write to chat
 	halt
 
 msg:	dw "now with string support!", 0
 cmsg:	dw c"and packed strings too!", 0
+hmsg:	dw 'h', 0
