c5426ff35dc25c958604e4d95deee0739fd59ada
xfnw <xfnw@riseup.net>
2025-10-16 13:09:34 -0400

    vancouver: check output in integration tests

--- crates/vancouver/tests/integration.rs
+++ crates/vancouver/tests/integration.rs
@@ -14,7 +14,7 @@ fn command_output(args: impl IntoIterator<Item = impl AsRef<OsStr>>) -> Output {
     Command::new(BIN).args(args).output().unwrap()
 }
 
-fn test_success(name: &str, code: i32) {
+fn test_exitcode(name: &str, output: &str, code: i32) -> String {
     let output = command_output([
         "check",
         "--lock",
@@ -23,21 +23,30 @@ fn test_success(name: &str, code: i32) {
         &format!("{name}vancouver.toml"),
         "--audits",
         &format!("{name}audits.toml"),
+        "--output",
+        output,
     ]);
-    dbg!(str::from_utf8(&output.stdout).unwrap());
+    let stdout = dbg!(str::from_utf8(&output.stdout).unwrap());
     dbg!(str::from_utf8(&output.stderr).unwrap());
     assert_eq!(output.status.code().unwrap(), code);
+    stdout.to_string()
 }
 
 #[test]
 fn workspace_check() {
-    test_success(WORKSPACE, 0);
+    assert_eq!(test_exitcode(WORKSPACE, "human", 0), "");
 }
 
 #[test]
 fn violation() {
-    test_success(
+    let stdout = test_exitcode(
         concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/violation-"),
+        "json",
         1,
+    );
+    assert_eq!(
+        stdout,
+        r#"{"dependencies":[{"fails":[{"needed":"yote","prev_version":"0.1.0","reason":"Violation"}],"name":"yip","status":"failed","version":"0.3.0"},{"fails":[{"needed":"yote","prev_version":null,"reason":"Violation"}],"name":"yap","status":"failed","version":"1.0.0"}],"total":2,"total_failed":2,"total_passed":0,"unused_exempts":[]}
+"#
     );
 }
