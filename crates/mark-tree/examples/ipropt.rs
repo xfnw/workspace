d314815c7bd218f3ef35631c29af4a3cc8088378
xfnw <xfnw@riseup.net>
2025-10-18 23:49:41 -0400

    mark-tree: add example

---
+++ crates/mark-tree/examples/ipropt.rs
@@ -0,0 +1,24 @@
+use mark_tree::{IpRange, MarkTree};
+
+fn main() {
+    let mut ipees = MarkTree::new();
+    for line in std::io::stdin().lines() {
+        let line = line.unwrap();
+        let Ok(range) = line.parse::<IpRange>() else {
+            eprintln!("ignoring {line:?}");
+            continue;
+        };
+        ipees.mark(range.iter());
+    }
+
+    eprintln!("optimizing...");
+    ipees.optimize();
+
+    ipees.traverse(|tree, bits| {
+        if !matches!(tree, MarkTree::AllMarked) {
+            return;
+        }
+        let range = IpRange::from_bits(bits).unwrap();
+        println!("{range}");
+    });
+}
