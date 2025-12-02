31aaf290b77a7c478f38c9aed6155b1bc70ee0a3
xfnw <xfnw@riseup.net>
2025-12-02 09:26:44 -0500

    gekker: hash lines

--- crates/gekker/src/main.rs
+++ crates/gekker/src/main.rs
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use std::collections::BTreeSet;
+use std::{collections::BTreeSet, hash::Hasher};
 use tokio::sync::{RwLock, mpsc};
 
 #[derive(Debug)]
@@ -16,6 +16,16 @@ struct State {
     clients: RwLock<Vec<Option<Client>>>,
     active: RwLock<BTreeSet<usize>>,
     callback: RwLock<mpsc::Sender<u64>>,
+}
+
+fn hash_line(nick: &[u8], command: &[u8], trail: &[u8]) -> u64 {
+    let mut hasher = std::hash::DefaultHasher::new();
+    hasher.write(nick);
+    hasher.write(b" ");
+    hasher.write(command);
+    hasher.write(b" ");
+    hasher.write(trail);
+    hasher.finish()
 }
 
 fn main() {
