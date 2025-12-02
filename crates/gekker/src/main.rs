38c028d1e3915ccb152155601745bee40ca17abe
xfnw <xfnw@riseup.net>
2025-12-01 19:47:01 -0500

    gekker: add some types

--- crates/gekker/src/main.rs
+++ crates/gekker/src/main.rs
@@ -2,6 +2,22 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use std::collections::BTreeSet;
+use tokio::sync::{RwLock, mpsc};
+
+#[derive(Debug)]
+struct Client {
+    nick: String,
+    sender: mpsc::Sender<Vec<u8>>,
+}
+
+#[derive(Debug)]
+struct State {
+    clients: RwLock<Vec<Option<Client>>>,
+    active: RwLock<BTreeSet<usize>>,
+    callback: RwLock<mpsc::Sender<u64>>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
