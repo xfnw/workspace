ef1ee29c4da4ed8457e53993dbdae3e7b5522a8f
xfnw <xfnw@riseup.net>
2026-05-16 09:27:34 -0400

    metrics-relay: parse kv list

--- crates/metrics-relay/src/influx.rs
+++ crates/metrics-relay/src/influx.rs
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: MIT
 
+use std::{collections::BTreeMap, str::FromStr};
+
 pub struct InfluxLine {
     name: String,
 }
@@ -63,4 +65,60 @@ fn parse_quoted(inp: &str) -> Option<(String, &str)> {
     let (s, rest) = parse_escaped_until(rest, |c| c == '"')?;
     assert_eq!(&rest[..1], "\"");
     Some((s, &rest[1..]))
+}
+
+fn parse_maybe_quoted(inp: &str, end_predicate: impl Fn(char) -> bool) -> Option<(String, &str)> {
+    parse_quoted(inp).or_else(|| parse_escaped_until(inp, end_predicate))
+}
+
+fn parse_kv<V: FromStr>(
+    inp: &str,
+    end_predicate: impl Fn(char) -> bool,
+) -> Option<(String, V, &str)> {
+    let (key, rest) = parse_maybe_quoted(inp, |c| end_predicate(c) || c == '=')?;
+    let Some(('=', rest)) = chomp(rest) else {
+        return None;
+    };
+    let (value, rest) = parse_maybe_quoted(rest, end_predicate)?;
+    Some((key, value.parse().ok()?, rest))
+}
+
+fn parse_kv_list<V: FromStr>(
+    mut inp: &str,
+    end_predicate: impl Fn(char) -> bool,
+) -> Option<(BTreeMap<String, V>, &str)> {
+    let mut out = BTreeMap::new();
+    loop {
+        let (key, value, rest) = parse_kv(inp, |c| end_predicate(c) || c == ',')?;
+        out.insert(key, value);
+        inp = match chomp(rest) {
+            Some((',', rest)) => rest,
+            Some((c, _)) => return end_predicate(c).then_some((out, rest)),
+            None => return Some((out, rest)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn kv_list() {
+        let (map, rest) = parse_kv_list::<f64>(
+            r#"bark=1,i\ have\ spaces=2,"im quoted"=3,"have a \backslash escaped quote: \""=4 meow"#,
+            |c| c == ' ',
+        )
+        .unwrap();
+        assert_eq!(rest, " meow");
+        let mut iter = map.iter();
+        assert_eq!(iter.next(), Some((&"bark".to_string(), &1f64)));
+        assert_eq!(
+            iter.next(),
+            Some((&"have a \\backslash escaped quote: \"".to_string(), &4f64))
+        );
+        assert_eq!(iter.next(), Some((&"i have spaces".to_string(), &2f64)));
+        assert_eq!(iter.next(), Some((&"im quoted".to_string(), &3f64)));
+        assert_eq!(iter.next(), None);
+    }
 }
