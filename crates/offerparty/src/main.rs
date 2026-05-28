bb759bfde79d4d2cfad2a4ab847c86bc69da2c63
xfnw <xfnw@riseup.net>
2026-05-27 21:47:21 -0400

    offerparty: listing directories

--- crates/offerparty/src/main.rs
+++ crates/offerparty/src/main.rs
@@ -3,8 +3,19 @@
 // SPDX-License-Identifier: EUPL-1.2
 
 use argh::FromArgs;
+use bytes::{Buf, Bytes};
+use http_body_util::{BodyExt, Empty};
+use hyper_util::client::legacy::{Client, connect::HttpConnector};
 use irctokens::Line;
-use std::{net::IpAddr, str::FromStr, sync::RwLock, time::Duration};
+use serde::Deserialize;
+use std::{
+    collections::HashMap,
+    fmt,
+    net::IpAddr,
+    str::FromStr,
+    sync::{Arc, Mutex, RwLock},
+    time::Duration,
+};
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpStream, ToSocketAddrs},
@@ -39,19 +50,33 @@ enum Error {
     Send,
     #[err(from)]
     Tokenise(irctokens::tokenise::Error),
+    #[err(from)]
+    ParseInt(std::num::ParseIntError),
+    UnknownId,
+    NotADirectory(String),
+    #[err(from)]
+    UrlParse(url::ParseError),
+    #[err(from)]
+    HttpClient(hyper_util::client::legacy::Error),
+    #[err(from)]
+    Hyper(hyper::Error),
+    #[err(from)]
+    Json(serde_json::Error),
 }
 
 struct Bot {
     send_raw: mpsc::UnboundedSender<Vec<u8>>,
     send_raw_receiver: AMutex<mpsc::UnboundedReceiver<Vec<u8>>>,
     send_message: mpsc::UnboundedSender<Message>,
+    http_client: Client<HttpConnector, Empty<Bytes>>,
     autojoin: Option<String>,
     copyparty_url: Url,
     myhost: RwLock<Option<IpAddr>>,
+    paths: Mutex<PathIdStore>,
 }
 
 impl Bot {
-    fn new(autojoin: Option<String>, copyparty_url: Url, delay: u64) -> Self {
+    fn new(autojoin: Option<String>, copyparty_url: Url, delay: u64) -> Arc<Self> {
         let (send_raw, send_raw_receiver) = mpsc::unbounded_channel();
         let send_raw_receiver = AMutex::new(send_raw_receiver);
         let (send_message, mut send_message_receiver) = mpsc::unbounded_channel();
@@ -69,18 +94,25 @@ impl Bot {
                 tokio::time::sleep(Duration::from_millis(delay)).await;
             }
         });
+        let http_client = Client::builder(hyper_util::rt::TokioExecutor::new()).build_http();
 
-        Self {
+        Arc::new(Self {
             send_raw,
             send_raw_receiver,
             send_message,
+            http_client,
             autojoin,
             copyparty_url,
             myhost: RwLock::new(None),
-        }
+            paths: Mutex::new(PathIdStore::new()),
+        })
     }
 
-    async fn connect_once(&self, nick: &str, addr: impl ToSocketAddrs) -> Result<(), Error> {
+    async fn connect_once(
+        self: &Arc<Self>,
+        nick: &str,
+        addr: impl ToSocketAddrs,
+    ) -> Result<(), Error> {
         let conn = TcpStream::connect(addr).await?;
         let (reader, mut writer) = tokio::io::split(conn);
         let mut reader = BufReader::new(reader);
@@ -167,7 +199,7 @@ impl Bot {
         Ok(())
     }
 
-    fn handle_privmsg(&self, line: &Line) -> Result<(), Error> {
+    fn handle_privmsg(self: &Arc<Self>, line: &Line) -> Result<(), Error> {
         let Some(source) = line
             .source
             .as_ref()
@@ -196,15 +228,92 @@ impl Bot {
             let (first, rest) = rest.split_once(' ').unwrap_or((rest, ""));
             match first.to_ascii_uppercase().as_str() {
                 "HELP" => {
-                    self.send_message(source.to_vec(), b"commands: XDCC HELP - this text, XDCC LIST [directory] - list stuff to download, XDCC SEND <number> - get a file".to_vec())?;
+                    self.send_message(source.to_vec(), b"commands: XDCC HELP - this text, XDCC LIST [number] - list stuff to download, XDCC SEND <number> - get a file".to_vec())?;
                 }
-                "LIST" => self.send_message(source.to_vec(), b"not implemented yet".to_vec())?,
+                "LIST" => {
+                    let target = source.to_vec();
+                    let rest = rest.to_string();
+                    let myself = Arc::clone(self);
+                    tokio::spawn(async move {
+                        myself
+                            .message_errors(&target, myself.do_list(&target, &rest))
+                            .await;
+                    });
+                }
                 "SEND" => {
                     self.send_message(source.to_vec(), b"also not implemented yet".to_vec())?;
                 }
                 _ => (),
             }
         }
+        Ok(())
+    }
+
+    async fn message_errors<F>(&self, target: &[u8], future: F)
+    where
+        F: Future<Output = Result<(), Error>> + Send,
+    {
+        if let Err(e) = future.await {
+            let formatted = format!("oh noes: {e}");
+            for line in formatted.lines() {
+                let mut line = line.as_bytes().to_vec();
+                if line.is_empty() {
+                    line.push(b' ');
+                }
+                if self.send_message(target.to_vec(), line).is_err() {
+                    break;
+                }
+            }
+        }
+    }
+
+    fn get_path(&self, num: &str) -> Result<String, Error> {
+        let num = if num.is_empty() {
+            0
+        } else {
+            usize::from_str(num)?
+        };
+
+        self.paths
+            .lock()
+            .unwrap()
+            .get_path(num)
+            .map(str::to_string)
+            .ok_or(Error::UnknownId)
+    }
+
+    async fn do_list(&self, target: &[u8], num: &str) -> Result<(), Error> {
+        let path = self.get_path(num)?;
+        if !path.is_empty() && !path.ends_with('/') {
+            return Err(Error::NotADirectory(path));
+        }
+        let mut url = self.copyparty_url.join(&path)?;
+        // ask copyparty for json
+        url.set_query(Some("ls"));
+        // FIXME: find a less nonsensical way to turn our url::Url into an http::Uri
+        let resp = self.http_client.get(url.as_str().parse().unwrap()).await?;
+        let body = resp.collect().await?.aggregate();
+        let dir: Directory = serde_json::from_reader(body.reader())?;
+
+        self.send_message(
+            target.to_vec(),
+            format!(
+                "{path:?} has {} directories and {} files:",
+                dir.dirs.len(),
+                dir.files.len()
+            )
+            .into_bytes(),
+        )?;
+
+        for entry in dir.dirs.iter().chain(dir.files.iter()) {
+            let fullpath = format!("{path}{}", entry.name);
+            let id = self.paths.lock().unwrap().generate_id(&fullpath);
+            self.send_message(
+                target.to_vec(),
+                format!("#{id} [{}] {fullpath}", Human(entry.size)).into_bytes(),
+            )?;
+        }
+
         Ok(())
     }
 }
@@ -222,6 +331,65 @@ impl Message {
             return None;
         }
         Some(Self { target, content })
+    }
+}
+
+struct PathIdStore {
+    by_path: HashMap<String, usize>,
+    by_id: Vec<String>,
+}
+
+impl PathIdStore {
+    fn new() -> Self {
+        let mut by_path = HashMap::new();
+        by_path.insert(String::new(), 0);
+        let by_id = vec![String::new()];
+        Self { by_path, by_id }
+    }
+    fn generate_id(&mut self, path: &str) -> usize {
+        if let Some(&id) = self.by_path.get(path) {
+            return id;
+        }
+        let new_id = self.by_id.len();
+        self.by_path.insert(path.to_string(), new_id);
+        self.by_id.push(path.to_string());
+        new_id
+    }
+    fn get_path(&self, id: usize) -> Option<&str> {
+        self.by_id.get(id).map(String::as_str)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Directory {
+    dirs: Vec<DirEntry>,
+    files: Vec<DirEntry>,
+}
+
+#[derive(Debug, Deserialize)]
+struct DirEntry {
+    #[serde(rename = "href")]
+    name: String,
+    #[serde(rename = "sz")]
+    size: u64,
+}
+
+struct Human(u64);
+
+impl fmt::Display for Human {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.0 == 0 {
+            return write!(f, "0");
+        }
+        match self.0.ilog2() {
+            60.. => write!(f, "{}EiB", self.0 >> 60),
+            50.. => write!(f, "{}PiB", self.0 >> 50),
+            40.. => write!(f, "{}TiB", self.0 >> 40),
+            30.. => write!(f, "{}GiB", self.0 >> 30),
+            20.. => write!(f, "{}MiB", self.0 >> 20),
+            10.. => write!(f, "{}KiB", self.0 >> 10),
+            _ => write!(f, "{}B", self.0),
+        }
     }
 }
 
